88e956d252c4a781796352e8d02dda78dd562ff6
samyguti089-art <samyguti089@gmail.com>
2026-09-15 18:12:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ app = FastAPI()
 # CORS
 # ============================================================
 origins = [
-    "https://congelador-lucky-fronted.vercel.app",
-    "https://fabrica-congelados.vercel.app",
+    "https://congelador-lucky-fronted.vercel.app",           # Tienda
+    "https://congelador-lucky-fronted-fabrica.vercel.app",   # Fábrica ✅
     "http://localhost:5173",
     "http://127.0.0.1:5173"
 ]
